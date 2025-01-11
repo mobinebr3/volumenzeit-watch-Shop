22db13863cf53fc196c9d07aa73b5118cbaa4823
Mobin Ebr <MobinEbr3@gmail.com>
2025-01-11 19:46:04 +0330

    testll

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,46 +4,45 @@ import { verifyPassword } from "@/utils/hashpassvaidtion";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import type { NextAuthOptions } from "next-auth";
-import { NextApiRequest, NextApiResponse } from "next";
 
-// NextAuthOptions تعریف شده با نوع‌دهی صحیح
+
 export const authOptions: NextAuthOptions = {
   session: { strategy: "jwt" },
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: { label: "email", type: "text" }, // نوع "string" به "text" تغییر یافته
-        password: { label: "password", type: "password" }, // نوع "string" به "password" تغییر یافته
+        email: { label: "email", type: "text" }, 
+        password: { label: "password", type: "password" }, 
       },
       authorize: async (credentials) => {
-        // اتصال به دیتابیس
+ 
         try {
           await ConnecttoDB();
         } catch (err) {
           throw new Error("Something went wrong while connecting to the database");
         }
 
-        // بررسی ایمیل و پسورد
+     
         const { email, password } = credentials || {};
         if (!email || !password) {
           throw new Error("Email and password are required");
         }
 
-        // پیدا کردن کاربر بر اساس ایمیل
+      
         const user = await User.findOne({ email });
 
         if (!user || !(await verifyPassword(password, user.password))) {
           throw new Error("Invalid email or password");
         }
 
-        // بازگشت داده‌های کاربر پس از تایید
+      
         return { email: user.email, name: user.name, id: user._id };
       },
     }),
   ],
   callbacks: {
-    // تعریف JWT callback
+
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
@@ -52,7 +51,7 @@ export const authOptions: NextAuthOptions = {
       }
       return token;
     },
-    // تعریف session callback
+   
     async session({ session, token }) {
       session.user = {
         email: token.email,
