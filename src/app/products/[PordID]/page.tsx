9c8d77551b9029156ded187270b64045175bea78
Mobin Ebr <MobinEbr3@gmail.com>
2025-01-22 23:36:15 +0330

    test ; fix:error in detailsPage

--- src/app/products/[PordID]/page.tsx
+++ src/app/products/[PordID]/page.tsx
@@ -1,6 +1,8 @@
+import Loader from "@/components/elements/Loader";
 import DetailPage from "@/components/templates/DetailPage";
 import { ProductfetchType } from "@/Types/typesw";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 async function Details({ params }: { params: Promise<{ PordID: number }> }) {
   const { PordID } = await params;
@@ -8,23 +10,22 @@ async function Details({ params }: { params: Promise<{ PordID: number }> }) {
     const res: any = await fetch(
       `${process.env.NEXTAUTH_URL}/api/products/details/${PordID}`
     );
-    if (!res) {
-      return <h1>somting worng</h1>;
-    }
- 
+   
 
   const { data }: ProductfetchType = await res.json();
 
 
   return (
-    <>
-      <div className="bg-purpuleColor h-40 w-full flex items-center justify-center ">
+    <> <div className="bg-purpuleColor h-40 w-full flex items-center justify-center ">
         <h1 className=" SpaceAGEfont text-5xl  max-md:text-4xl text-white">
           {" "}
           Details
         </h1>
       </div>
+    <Suspense fallback={<Loader />} >
+   
       <DetailPage data={data} />
+    </Suspense>
     </>
   ); } catch (error) {
     redirect('/error')
