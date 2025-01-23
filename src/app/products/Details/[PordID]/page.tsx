fa04fe595843637402ce64229a3cf9a81ac6a52f
Mobin Ebr <MobinEbr3@gmail.com>
2025-01-23 14:59:42 +0330

    test

--- src/app/products/Details/[PordID]/page.tsx
+++ src/app/products/Details/[PordID]/page.tsx
@@ -4,18 +4,18 @@ import { ProductfetchType } from "@/Types/typesw";
 
 import { Suspense } from "react";
 
-async function Details({ params }: { params: Promise<{ PordID: string }> }) {
+async function Details({ params }: { params:{ PordID: string } }) {
   try {
-    const { PordID } = await params;
+    const { PordID } = params;
 
     const res= await fetch(
       `${process.env.NEXTAUTH_URL}/api/products/details/${PordID}`
     );
 
-
+    if(!res.ok)return <h1>somthing error in connection</h1>
   const { data }: ProductfetchType = await res.json();
 
-  if(!data)return <h1>somthing error in connection</h1>
+
   return (
     <> <div className="bg-purpuleColor h-40 w-full flex items-center justify-center ">
         <h1 className=" SpaceAGEfont text-5xl  max-md:text-4xl text-white">
