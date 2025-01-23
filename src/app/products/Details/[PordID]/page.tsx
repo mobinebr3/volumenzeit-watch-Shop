5ebef5bf7558854a6d4bddcbe1da52c4bb6bba8f
Mobin Ebr <MobinEbr3@gmail.com>
2025-01-23 14:26:56 +0330

    test

--- src/app/products/Details/[PordID]/page.tsx
+++ src/app/products/Details/[PordID]/page.tsx
@@ -4,18 +4,18 @@ import { ProductfetchType } from "@/Types/typesw";
 
 import { Suspense } from "react";
 
-async function Details({ params }: { params: Promise<{ PordID: string }> }) {
-  try {
-    const { PordID } = await params;
+async function Details() {
 
-    const res= await fetch(
-      `${process.env.NEXTAUTH_URL}/api/products/details/${PordID}`
-    );
+  //   const { PordID } = await params;
+
+  //   const res= await fetch(
+  //     `${process.env.NEXTAUTH_URL}/api/products/details/${PordID}`
+  //   );
 
 
-  const { data }: ProductfetchType = await res.json();
+  // const { data }: ProductfetchType = await res.json();
 
-  if(!data)return <h1>somthing error in connection</h1>
+  // if(!data)return <h1>somthing error in connection</h1>
   return (
     <> <div className="bg-purpuleColor h-40 w-full flex items-center justify-center ">
         <h1 className=" SpaceAGEfont text-5xl  max-md:text-4xl text-white">
@@ -23,15 +23,12 @@ async function Details({ params }: { params: Promise<{ PordID: string }> }) {
           Details
         </h1>
       </div>
-    <Suspense fallback={<Loader />} >
+    {/* <Suspense fallback={<Loader />} >
    
       <DetailPage data={data} />
-    </Suspense>
+    </Suspense> */}
     </>
-  ); } catch (error:any) {
-    console.log(error)
-   return <code>error:{error}</code>
-  }
+  )
   
 }
 
