169d0dac1a17bdb41d334a53b7a7c72be6c6543b
Mobin Ebr <MobinEbr3@gmail.com>
2025-01-28 11:26:32 +0330

    fix error in deails page

--- src/app/products/Details/[PordID]/page.tsx
+++ src/app/products/Details/[PordID]/page.tsx
@@ -4,35 +4,32 @@ import { ProductfetchType } from "@/Types/typesw";
 
 import { Suspense } from "react";
 
-export default async function Details({params}:any) {
-  const id =(await params).PordID
+export default async function Details({ params }: any) {
+  const id = (await params).PordID;
 
   try {
-  
-
-    const res= await fetch(
+    const res = await fetch(
       `${process.env.NEXTAUTH_URL}/api/products/details/${id}`
     );
 
-    if(!res.ok)return <h1>somthing error in connection</h1>
-  const { data }: ProductfetchType = await res.json();
+    const { data }: ProductfetchType = await res.json();
+    if (!data) return <h1>somthing error in connection</h1>;
 
-
-  return (
-    <> <div className="bg-purpuleColor h-40 w-full flex items-center justify-center ">
-        <h1 className=" SpaceAGEfont text-5xl  max-md:text-4xl text-white">
-          {" "}
-          Details
-        </h1>
-      </div>
-    <Suspense fallback={<Loader />} >
-   
-      <DetailPage data={data} />
-    </Suspense>
-    </>
-  ); } catch (error:any) {
-   return <code>error:{error}</code>
+    return (
+      <>
+        {" "}
+        <div className="bg-purpuleColor h-40 w-full flex items-center justify-center ">
+          <h1 className=" SpaceAGEfont text-5xl  max-md:text-4xl text-white">
+            {" "}
+            Details
+          </h1>
+        </div>
+        <Suspense fallback={<Loader />}>
+          <DetailPage data={data} />
+        </Suspense>
+      </>
+    );
+  } catch (error: any) {
+    return <code>error:{error}</code>;
   }
-  
 }
-
