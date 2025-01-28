1fbbcb2271712aa615dc93f7a5aeda343a6cd673
Mobin Ebr <MobinEbr3@gmail.com>
2025-01-28 17:29:59 +0330

    test

--- src/app/products/Details/[PordID]/page.tsx
+++ src/app/products/Details/[PordID]/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 
 export default async function Details({ params }: any) {
   const id =  (await params).PordID
-
+console.log(id)
   try {
     const res  = await fetch(
       `${process.env.NEXTAUTH_URL}/api/products/details/${id}`
@@ -14,7 +14,7 @@ export default async function Details({ params }: any) {
 console.log(res,';res')
 
     const {data}:ProductfetchType = await res.json();
-
+console.log(data,':data')
     return (
       <>
         {" "}
